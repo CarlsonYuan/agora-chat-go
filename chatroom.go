1bb5c77745d6329373543d18419c56ca1c60a7e7
CarlsonYuan <carlsonyuandev@gmail.com>
2023-11-21 11:29:05 +0800

    feat: chatroom 'Manage chat rooms'

--- chatroom.go
+++ chatroom.go
@@ -10,15 +10,34 @@ import (
 )
 
 type Chatroom struct {
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Maxusers    int      `json:"maxusers"`
-	Owner       string   `json:"owner"`
-	Members     []string `json:"members"`
+	Members           []string `json:"members,omitempty"`
+	Owner             string   `json:"owner,omitempty"`
+	ID                string   `json:"id,omitempty"`
+	Name              string   `json:"name,omitempty"`
+	Description       string   `json:"description,omitempty"`
+	Membersonly       bool     `json:"membersonly,omitempty"`
+	Allowinvites      bool     `json:"allowinvites,omitempty"`
+	InviteNeedConfirm bool     `json:"invite_need_confirm,omitempty"`
+	Maxusers          int      `json:"maxusers,omitempty"`
+	Created           int64    `json:"created,omitempty"`
+	Custom            string   `json:"custom,omitempty"`
+	Mute              bool     `json:"mute,omitempty"`
+	Scale             string   `json:"scale,omitempty"`
+	AffiliationsCount int      `json:"affiliations_count,omitempty"`
+	Disabled          bool     `json:"disabled,omitempty"`
+	Affiliations      []struct {
+		Member string `json:"member,omitempty"`
+		Owner  string `json:"owner,omitempty"`
+	} `json:"affiliations,omitempty"`
+	Public bool `json:"public,omitempty"`
+}
+type RestHandleResult struct {
+	ID     string `json:"id,omitempty"`
+	Result bool   `json:"result,omitempty"`
 }
 
 type ChatroomResponse struct {
-	Data []CreateUserError `json:"data"`
+	Data RestHandleResult `json:"data"`
 	Response
 }
 
@@ -34,44 +53,76 @@ type QueryChatroomsRequest struct {
 	Pagination *PaginationParamsRequest
 }
 
-func (c *Client) QueryChatrooms(ctx context.Context, q *QueryUsersRequest) (*interface{}, error) {
+type QueryChatroomsResponseItem struct {
+	AffiliationsCount int         `json:"affiliations_count"`
+	Disabled          interface{} `json:"disabled"`
+	ID                string      `json:"id"`
+	Name              string      `json:"name"`
+	Owner             string      `json:"owner"`
+}
+type QueryChatroomsResponse struct {
+	Data []*QueryChatroomsResponseItem `json:"data"`
+	Response
+}
+
+// QueryChatrooms retrieves the basic information of all chat rooms under the app by page.
+func (c *Client) QueryChatrooms(ctx context.Context, q *QueryChatroomsRequest) (*QueryChatroomsResponse, error) {
 
 	values := url.Values{}
 	values.Add("limit", fmt.Sprint(q.Pagination.Limit))
 	values.Add("cursor", q.Pagination.Cursor)
 
-	var resp interface{}
+	var resp QueryChatroomsResponse
 	err := c.makeRequest(ctx, http.MethodGet, "chatrooms", values, nil, &resp)
 	return &resp, err
 }
 
-func (c *Client) QueryUserJoinedChatrooms(ctx context.Context, userID string, q *QueryUsersRequest) (*interface{}, error) {
+// QueryUserJoinedChatrooms retrieves all the chat rooms that a user joins.
+func (c *Client) QueryUserJoinedChatrooms(ctx context.Context, userID string, q *QueryChatroomsRequest) (*QueryChatroomsResponse, error) {
 
 	values := url.Values{}
 	values.Add("limit", fmt.Sprint(q.Pagination.Limit))
 	values.Add("cursor", q.Pagination.Cursor)
 
-	var resp interface{}
+	var resp QueryChatroomsResponse
 	p := path.Join("users", url.PathEscape(userID), "joined_chatrooms")
 	err := c.makeRequest(ctx, http.MethodGet, p, values, nil, &resp)
 	return &resp, err
 }
 
-func (c *Client) QuerySpecifiedChatrooms(ctx context.Context, chatroomIDs ...string) (*interface{}, error) {
+type QuerySpecifiedChatroomsResponse struct {
+	Data []*Chatroom `json:"data"`
+	Response
+}
 
-	var resp interface{}
+// QuerySpecifiedChatrooms retrieves the detailed information of one or more specified chat rooms.
+func (c *Client) QuerySpecifiedChatrooms(ctx context.Context, chatroomIDs ...string) (*QuerySpecifiedChatroomsResponse, error) {
+	var resp QuerySpecifiedChatroomsResponse
 	chatroomIDsString := strings.Join(chatroomIDs, ",")
 	p := path.Join("chatrooms", url.PathEscape(chatroomIDsString))
 	err := c.makeRequest(ctx, http.MethodGet, p, nil, nil, &resp)
 	return &resp, err
 }
-func (c *Client) UpdateChatroom(ctx context.Context, chatroom *Chatroom) (*interface{}, error) {
-	req := chatroom
-	var resp interface{}
-	err := c.makeRequest(ctx, http.MethodPut, "chatrooms", nil, req, &resp)
+
+type UpdateChatroomRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Maxusers    int    `json:"maxusers"`
+}
+type UpdateChatroomResponse struct {
+	Data *map[string]interface{} `json:"data"`
+	Response
+}
+
+// UpdateChatroom modifies the information of the specified chat room.
+func (c *Client) UpdateChatroom(ctx context.Context, chatroomID string, req *UpdateChatroomRequest) (*UpdateChatroomResponse, error) {
+	var resp UpdateChatroomResponse
+	p := path.Join("chatrooms", url.PathEscape(chatroomID))
+	err := c.makeRequest(ctx, http.MethodPut, p, nil, req, &resp)
 	return &resp, err
 }
 
+// DeleteChatroom deletes the specified chat room
 func (c *Client) DeleteChatroom(ctx context.Context, chatroomID string) (*interface{}, error) {
 	var resp interface{}
 	p := path.Join("chatrooms", url.PathEscape(chatroomID))
@@ -79,8 +130,14 @@ func (c *Client) DeleteChatroom(ctx context.Context, chatroomID string) (*interf
 	return &resp, err
 }
 
-func (c *Client) QueryChatroomAnnouncement(ctx context.Context, chatroomID string) (*interface{}, error) {
-	var resp interface{}
+type QueryChatroomAnnouncementResponse struct {
+	Data *map[string]interface{} `json:"data"`
+	Response
+}
+
+// QueryChatroomAnnouncement retrieves the announcement text for the specified chat room.
+func (c *Client) QueryChatroomAnnouncement(ctx context.Context, chatroomID string) (*QueryChatroomAnnouncementResponse, error) {
+	var resp QueryChatroomAnnouncementResponse
 	p := path.Join("chatrooms", url.PathEscape(chatroomID), "announcement")
 	err := c.makeRequest(ctx, http.MethodGet, p, nil, nil, &resp)
 	return &resp, err
@@ -90,8 +147,14 @@ type UpdateChatroomAnnouncementRequest struct {
 	Announcement string `json:"announcement"`
 }
 
-func (c *Client) UpdateChatroomAnnouncement(ctx context.Context, chatroomID string, req UpdateChatroomAnnouncementRequest) (*interface{}, error) {
-	var resp interface{}
+type UpdateChatroomAnnouncementResponse struct {
+	Data *RestHandleResult `json:"data"`
+	Response
+}
+
+// UpdateChatroomAnnouncement modifies the announcement text of the specified chat room.
+func (c *Client) UpdateChatroomAnnouncement(ctx context.Context, chatroomID string, req *UpdateChatroomAnnouncementRequest) (*UpdateChatroomAnnouncementResponse, error) {
+	var resp UpdateChatroomAnnouncementResponse
 	p := path.Join("chatrooms", url.PathEscape(chatroomID), "announcement")
 	err := c.makeRequest(ctx, http.MethodPost, p, nil, req, &resp)
 	return &resp, err
